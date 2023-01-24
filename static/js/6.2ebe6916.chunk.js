c04e83d20844de640fefe9ec093a3099a6446c0f
Abdullah Ahmed <abdullahejazj@gmail.com>
2023-01-24 22:29:26 +0500

    changes

--- static/js/6.2ebe6916.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpax-movies"]=this["webpackJsonpax-movies"]||[]).push([[6],{106:function(e,t,s){"use strict";s.r(t),s.d(t,"default",(function(){return b}));var c=s(1),o=s(2),i=s(36),a=s(43),n=s(34),r=s(12),u=s(0);function b(){var e=Object(o.g)().category,t="movies"===e;return Object(c.useEffect)((function(){Object(r.a)()}),[e]),Object(u.jsxs)(i.a,{title:t?"Movies":"TV Shows",children:[Object(u.jsx)(a.b,{category:t?"Movies":"TV Shows"}),Object(u.jsx)(n.a,{category:t?"movie":"tv"})]})}}}]);
-//# sourceMappingURL=6.2ebe6916.chunk.js.map
