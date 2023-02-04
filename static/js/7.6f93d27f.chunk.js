ce04e8f7aeea1c57f9a71bf7481837f3ff9f6f6d
Ahmed <abdullahejazj@gmail.com>
2023-02-04 14:53:26 +0500

    build final

---
+++ static/js/7.6f93d27f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpax-movies"]=this["webpackJsonpax-movies"]||[]).push([[7],{109:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return s}));var i=e(0);function s(n){return Object(i.jsx)("div",{children:"Not found"})}}}]);
+//# sourceMappingURL=7.6f93d27f.chunk.js.map
